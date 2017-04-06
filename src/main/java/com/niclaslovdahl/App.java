bd37dc472549fdf0cad187ad5fe52a759215a0eb
Niclas Lovdahl <niclas.lovdahl@gmail.com>
2017-04-06 17:58:38 +0200

    fully working app, added gui and optionpanes

--- src/main/java/com/niclaslovdahl/App.java
+++ src/main/java/com/niclaslovdahl/App.java
@@ -1,7 +1,17 @@
 package com.niclaslovdahl;
 
+import java.awt.Desktop;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import javax.swing.ImageIcon;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -11,7 +21,6 @@ import org.jsoup.select.Elements;
 import com.machinepublishers.jbrowserdriver.JBrowserDriver;
 import com.machinepublishers.jbrowserdriver.Settings;
 import com.machinepublishers.jbrowserdriver.Timezone;
-import com.sun.media.sound.SoftSynthesizer;
 
 /**
  * Main class.
@@ -20,38 +29,85 @@ import com.sun.media.sound.SoftSynthesizer;
  * 
  */
 public class App {
-	public static void main(String[] args) throws IOException {
-		JBrowserDriver driver = new JBrowserDriver(Settings.builder().timezone(Timezone.EUROPE_ATHENS).build());
-		driver.get("https://www.discogs.com/seller/franciesco/profile");
-		String loadedPage = driver.getPageSource();
 
-		// Item page link parsed
-		Document document = Jsoup.parse(loadedPage);
-		Elements elements = document.getElementsByClass("item_description_title");
+	private JBrowserDriver driver;
+	private String link = "";
+	private String finalString = "http://www.youtube.com/watch_videos?video_ids=";
+	private JFrame frame;
+
+	public App() {
+		this.link = JOptionPane.showInputDialog("Insert discogs link");
+		this.frame = new JFrame("Discogs Video Parser");
+		ImageIcon loading = new ImageIcon("res/ajax-loader.gif");
+		frame.add(new JLabel("loading... ", loading, JLabel.CENTER));
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300, 200);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		driver = new JBrowserDriver(Settings.builder().timezone(Timezone.EUROPE_ATHENS).build());
+	}
+
+	public void run() throws MalformedURLException {
+		Document doc = parsePage(link);
+		Elements elements = doc.getElementsByClass("item_description_title");
 
 		for (Element element : elements) {
-			// Item page load
-			System.out.println(element.attr("href"));
-			driver.get("https://discogs.com" + element.attr("href"));
-			Document doc = Jsoup.parse(driver.getPageSource());
-			// Release page link parsed
-			Elements ele = doc.getElementsContainingText("View Release Page");
-			System.out.println(ele.attr("href"));
-			
-			driver.get("https://discogs.com" + ele.attr("href"));
-			Document doc2 = Jsoup.parse(driver.getPageSource());	
-			
-			Element element2 = doc2.getElementById("youtube_player_placeholder");
-			
-			System.out.println(element2.attr("data-video-ids"));
-			String links = element2.attr("data-video-ids");
-			String[] links2 = links.split(",");
-			for (String string : links2) {
-				System.out.println(string);
-			}
-			break;
-		}
+			Document doc2 = parsePage("https://discogs.com" + element.attr("href"));
+			Elements ele = doc2.getElementsContainingText("View Release Page");
 
+			Document doc3 = parsePage("https://discogs.com" + ele.attr("href"));
+			Element ele2 = doc3.getElementById("youtube_player_placeholder");
+			if (ele2 != null) {
+				String links = ele2.attr("data-video-ids");
+				String[] links2 = links.split(",");
+				for (String string : links2) {
+					finalString += string + ",";
+				}
+			}
+		}
+		removeLastFromFinal();
+		openWebpage(new URL(finalString));
+		frame.dispose();
+		final JDialog dialog = new JDialog();
+		dialog.setAlwaysOnTop(true);
+		JOptionPane.showMessageDialog(dialog, "Playlist successfully created. Enjoy!");
 		driver.quit();
+	}
+
+	public Document parsePage(String link) {
+		driver.get(link);
+		Document doc = Jsoup.parse(driver.getPageSource());
+		return doc;
+	}
+
+	public void removeLastFromFinal() {
+		if (finalString != null && finalString.length() > 0 && finalString.charAt(finalString.length() - 1) == ',') {
+			finalString = finalString.substring(0, finalString.length() - 1);
+		}
+	}
+
+	public static void openWebpage(URI uri) {
+		Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+		if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+			try {
+				desktop.browse(uri);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void openWebpage(URL url) {
+		try {
+			openWebpage(url.toURI());
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		App app = new App();
+		app.run();
 	}
 }
